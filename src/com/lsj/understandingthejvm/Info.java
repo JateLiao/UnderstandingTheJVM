61743c4b36a507844a55f9b069e5e4924237ef98
Jate Liao <3746826117@qq.com>
2018-07-12 23:57:25 +0800

    提交项目初始结构

---
+++ src/com/lsj/understandingthejvm/Info.java
@@ -0,0 +1,12 @@
+package com.lsj.understandingthejvm;
+
+/**
+ * @Title: ${FILE_NAME}
+ * @Company: com.lsj
+ * @Package com.lsj.understandingthejvm
+ * @Description: ${TODO}
+ * @Author liao
+ * @Createtine 2018/7/1223:23
+ */
+public class Info {
+}
