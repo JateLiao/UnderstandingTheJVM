6ec7c3229859e95862cfd51056d47312fe1217b6
Jate Liao <3746826117@qq.com>
2018-07-27 00:16:41 +0800

    code 6-1

---
+++ src/com/lsj/understandingthejvm/part3/chapter6/Code6_1.java
@@ -0,0 +1,17 @@
+package com.lsj.understandingthejvm.part3.chapter6;
+
+/**
+ * @Title: ${FILE_NAME}
+ * @Company: com.lsj
+ * @Package com.lsj.understandingthejvm.part2.chapter3
+ * @Description: ${TODO}
+ * @Author liao
+ * @Createtine 2018/7/2623:54
+ */
+public class Code6_1 {
+    private int m;
+    
+    public int inc() {
+        return m + 1;
+    }
+}
